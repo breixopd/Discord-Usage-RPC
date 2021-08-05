a72cddc1b0475e77e20c5e441180592569ddd32f
breixopd <32527555+breixopd@users.noreply.github.com>
2021-08-05 14:12:18 +0100

    Added additional error checking

--- jsontest.py
+++ jsontest.py
@@ -9,10 +9,22 @@ def importconfig():
         print("'client_id' needs to be the number in the developer portal!")
         exit()
     state = data["state"]
+    if state == "":
+        state = "Usage RPC: bit.ly/37Br9ml"
     large_image = data["large_image"]
+    if large_image == "" or large_image == "YOUR_LARGE_IMAGE_ID":
+        print("You need to enter something in 'large_image'!")
+        exit()
     small_image = data["small_image"]
+    if small_image == "" or small_image == "YOUR_SMALL_IMAGE_ID":
+        print("You need to enter something in 'large_image'!")
+        exit()
     large_text = data["large_text"]
+    if large_text == "" or large_text == "YOUR_LARGE_IMAGE_TEXT":
+        large_text = "Star on github!"
     small_text = data["small_text"]
+    if small_text == "" or small_text == "YOUR_SMALL_IMAGE_TEXT":
+        small_text = "By b2splashyy"
     os = data["os"].upper()
     if os == "TRUE":
         os = 1
@@ -30,15 +42,33 @@ def importconfig():
                 exit()
             elif buttonnum == 1:
                 button1_text = data["button1_text"]
+                if button1_text == "" or button1_text == "BUTTON_1_TEXT":
+                    print("Enter something for 'button1_text'!")
+                    exit()
                 button1_url = data["button1_url"]
+                if button1_url == "" or button1_url == "BUTTON_1_URL":
+                    print("Enter something for 'button1_url'!")
+                    exit()
                 button2_text = 0
                 button2_url = 0
                 return client_id, state, large_image, small_image, large_text, small_text, button1_text, button1_url, button2_text, button2_url, os
             elif buttonnum == 2:
                 button1_text = data["button1_text"]
+                if button1_text == "" or button1_text == "BUTTON_1_TEXT":
+                    print("Enter something for 'button1_text'!")
+                    exit()
                 button1_url = data["button1_url"]
+                if button1_url == "" or button1_url == "BUTTON_1_URL":
+                    print("Enter something for 'button1_url'!")
+                    exit()
                 button2_text = data["button2_text"]
+                if button2_text == "" or button2_text == "BUTTON_2_TEXT":
+                    print("Enter something for 'button2_text'!")
+                    exit()
                 button2_url = data["button2_url"]
+                if button2_url == "" or button2_url == "BUTTON_2_URL":
+                    print("Enter something for 'button2_url'!")
+                    exit()
                 return client_id, state, large_image, small_image, large_text, small_text, button1_text, button1_url, button2_text, button2_url, os
         except ValueError:
             print("Please enter a number in 'buttonnum'!")
